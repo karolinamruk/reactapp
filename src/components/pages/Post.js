58f5dedc3ba9a60a7225b40a75bef1a2d632fd92
Karolina Mruk <kmruk88@gmail.com>
2024-07-31 16:15:37 +0200

    Add delete function and modal

--- src/components/pages/Post.js
+++ src/components/pages/Post.js
@@ -1,11 +1,21 @@
-import { useSelector } from 'react-redux';
-import { useParams, Link } from 'react-router-dom';
-import { Button, Container } from 'react-bootstrap';
-import { getPostById } from '../../redux/postsRedux';
+import { useState } from 'react';
+import { useSelector, useDispatch } from 'react-redux';
+import { useParams, Link, Navigate } from 'react-router-dom';
+import { Button, Container, Modal } from 'react-bootstrap';
+import { getPostById, deletePost } from '../../redux/postsRedux';
 
 const Post = () => {
   const { id } = useParams();
   const post = useSelector((state) => getPostById(state, id));
+  const dispatch = useDispatch();
+  const [showModal, setShowModal] = useState(false);
+
+  const handleDelete = () => {
+    dispatch(deletePost(id));
+    setShowModal(false);
+  };
+
+  if (!post) return <Navigate to="/" />;
 
   return (
     <Container>
@@ -15,7 +25,9 @@ const Post = () => {
           <Button variant="primary" as={Link} to={`/post/edit/${post.id}`}>
             Edit
           </Button>{' '}
-          <Button variant="danger">Delete</Button>
+          <Button variant="danger" onClick={() => setShowModal(true)}>
+            Delete
+          </Button>
         </div>
       </div>
       <p>
@@ -25,6 +37,21 @@ const Post = () => {
         <strong>Published:</strong> {post.publishedDate}
       </p>
       <p>{post.content}</p>
+
+      <Modal show={showModal} onHide={() => setShowModal(false)}>
+        <Modal.Header closeButton>
+          <Modal.Title>Confirm Delete</Modal.Title>
+        </Modal.Header>
+        <Modal.Body>Are you sure you want to delete this post?</Modal.Body>
+        <Modal.Footer>
+          <Button variant="secondary" onClick={() => setShowModal(false)}>
+            Cancel
+          </Button>
+          <Button variant="danger" onClick={handleDelete}>
+            Confirm
+          </Button>
+        </Modal.Footer>
+      </Modal>
     </Container>
   );
 };
--- src/redux/postsRedux.js
+++ src/redux/postsRedux.js
@@ -6,10 +6,17 @@ export const getPostById = ({ posts }, postId) =>
 
 // actions
 const createActionName = (actionName) => `app/posts/${actionName}`;
+const DELETE_POST = 'app/posts/DELETE_POST';
 
 // action creators
+export const deletePost = (id) => ({ type: DELETE_POST, payload: id });
+//reducer
+
 const postsReducer = (statePart = [], action) => {
   switch (action.type) {
+    case DELETE_POST:
+      return statePart.filter((post) => post.id !== action.payload);
+
     default:
       return statePart;
   }
